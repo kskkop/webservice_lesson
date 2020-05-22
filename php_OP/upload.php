498bd8fabb05dffeeec6b2998d738e0cd4e73979
石澤啓介 <keisuke96125@ishizawakeisukenoMacBook-Pro.local>
2020-05-23 03:09:52 +0900

    push
    今度こそ

--- php_OP/upload.php
+++ php_OP/upload.php
@@ -1,7 +1,7 @@
 <?php 
 if(!empty($_FILES)){
     //$file = $_FILES['image']; →array(5){["name"] => "画像パス",["type"] => "image/jpeg",["tmp_name"] => "サーバー側にある一時的に保存されている場所"}
-    //var_dumpで$file,$_FILESの中身を要確認
+    //var_dumpで$file,$_FILESの中身を要確認する
     $upload_path = 'images/'.$file['name'];
 
     $rst = move_uploaded_file($file['tmp_name'],$upload_path);
