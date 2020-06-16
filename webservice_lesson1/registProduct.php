0fe9f63378238d1f38d6a1db692e43eda42c1854
石澤啓介 <keisuke96125@gmail.com>
2020-06-17 00:12:05 +0900

    20200616
    registProduct.php

--- webservice_lesson1/registProduct.php
+++ webservice_lesson1/registProduct.php
@@ -185,8 +185,90 @@ require('head.php');
                             ?>
                         </select>
                     </label>
+                    <div class="area-msg">
+                        <?php
+                        if (!empty($err_msg['category_id'])) echo $err_msg['category_id'];
+                        ?>
+                    </div>
+                    <label class="<?php if (!empty($err_msg['common'])) echo 'err' ?>">
+                        詳細
+                        <textarea name="comment" id="js-count" cols="30" rows="10" style="height:150px;"><?php echo getFormData('comment'); ?></textarea>
+                    </label>
+                    <p class="counter-text"><span id="js-count-view">0</span>/500文字</p>
+                    <div class="area-msg">
+                        <?php
+                        if (!empty($err_msg['comment'])) echo $err_msg['comment'];
+                        ?>
+                    </div>
+                    <label style="text-align:left;" class="<?php if (!empty($err_msg['price'])) echo 'err'; ?>">
+                        金額<span class="label-require">必須</span>
+                        <div class="form-group">
+                            <input type="text" name="price" style="width:150px;" placeholder="50,000" value="<?php echo (!empty(getFormData('price'))) ? getFormData('price') : 0; ?>"><span class="option">円</span>
+                        </div>
+                    </label>
+                    <div class="area-msg">
+                        <?php
+                        if (!empty($err_msg['price'])) echo $err_msg['price'];
+                        ?>
+                    </div>
+                    <div style="overflow:hidden;">
+                        <div class="imgDrop-container">
+                            画像１
+                            <label class="area-drop"> <?php if (!empty($err_msg['prc1'])) echo 'err'; ?>
+                                <input type="hidden" name="MAX_FILE_SIZE" value="3145728">
+                                <input type="file" name="pic1" class="input-file">
+                                <img src="<?php echo getFormData('pic1'); ?>" alt="" class="prev-img" style="<?php if (empty(getFormData('pic1'))) echo 'display:none;'; ?>">
+                                ドラッグ&ドロップ
+                            </label>
+                            <div class="area-msg">
+                                <?php
+                                if (!empty($err_msg['pic1'])) echo $err_msg['pic1'];
+                                ?>
+                            </div>
+                        </div>
+                        <div class="imgDrop-container">
+                            画像2
+                            <label class="area-drop"><?php if(!empty($err_msg['pic2'])) echo 'err';?>
+                                <input type="hidden" name="MAX_FILE_SIZE" value="3145728">
+                                <input type="file" name="pic2" class="input-file">
+                                <img src="<?php echo getFormData('pic2'); ?>" alt="" class="prev-img" style="<?php if(empty(getFormData('pic2'))) echo 'display:none;' ?>">
+                                ドラッグ＆ドロップ
+                            </label>
+                            <div class="area-msg">
+                                <?php
+                                if(!empty($err_msg['pic2'])) echo $err_msg['pic2'];
+                                ?>
+                            </div>
+                        </div>
+                        <div class="imgDrop-container">
+                            画像3
+                            <label class="area-drop"><?php if(!empty($err_msg['pic2'])) echo 'err';?>
+                                <input type="hidden" name="MAX_FILE_SIZE" value="3145728">
+                                <input type="file" name="pi32" class="input-file">
+                                <img src="<?php echo getFormData('pic3'); ?>" alt="" class="prev-img" style="<?php if(empty(getFormData('pic3'))) echo 'display:none;' ?>">
+                                ドラッグ＆ドロップ
+                            </label>
+                            <div class="area-msg">
+                                <?php
+                                if(!empty($err_msg['pic3'])) echo $err_msg['pic3'];
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="btn-container">
+                        <input type="submit" class="btn btn-mid" value="<?php echo(!$edit_flg) ? '出品する' :'更新する' ?>">
+                    </div>
                 </form>
             </div>
         </section>
+
+        <!--サイドバー-->
+        <?php
+        require('sidebar_mypage.php');
+        ?>
     </div>
-</body>
+     <!---footer-->
+     <?php
+     require('footer.php');
+     ?>
