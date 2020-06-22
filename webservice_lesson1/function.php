f4cc2fb4e1fccf90ccffaeae1ed49c07f9dc6342
石澤啓介 <keisuke96125@gmail.com>
2020-06-22 23:33:04 +0900

    20200622
    function.php

--- webservice_lesson1/function.php
+++ webservice_lesson1/function.php
@@ -347,7 +347,7 @@ function getProductOne($p_id){
   //例外処理
   try{
     //DBへ接続
-    $dbh = dbConnext();
+    $dbh = dbConnect();
     //SQL文作成
     $sql = 'SELECT p.id , p.name , p.comment, p.price, p.pic1, p.pic2, p.pic3, p.user_id, p.create_date, p.update_date, c.name AS category
             FROM product AS p LEFT JOIN category AS c ON p.category_id = c.id WHERE p.id = :p_id AND p.delete_flg = 0 AND c.delete_flg = 0';
@@ -511,6 +511,81 @@ function uploadImg($file,$key){
       global $err_msg;
       $err_msg[$key] = $e->getMessage();
     }
+  }
+}
+//ページング
+// $currentPageNum : 現在のページ数
+//$totalPageNum : 総ページ数
+//$link : 検索用GETパラメータリンク
+//$pageColNum : ページネーション表示数
+function pagination($currentPageNum,$totalPageNum,$link = '',$pageColNum = 5){
+    // 現在のページが、総ページ数と同じ　かつ　総ページ数が表示項目数以上なら、左にリンク４個出す
+    if($currentPageNum == $totalPageNum && $totalPageNum >= $pageColNum){
+      $minPageNum = $currentPageNum -4;
+      $maxPageNum = $currentPageNum;
+      
+      //現在のページが、総ページ数の１ページ前なら、左にリンク３個、右に１個出す
+    }elseif($currentPageNum == ($totalPageNum-1) && $totalPageNum >= $pageColNum){
+      $minPageNum = $currentPageNum -3;
+      $maxPageNum = $currentPageNum +1;
+
+      //現ページが２の場合は左にリンク１個、右にリンク３個出す。
+    }elseif($currentPageNum == 2 && $totalPageNum >= $pageColNum){
+      $minPageNum = $currentPageNum -1;
+      $maxPageNum = $currentPageNum +3;
+
+      //現ページが１の場合は左に何も出さない。右に５個出す。
+    }elseif($currentPageNum == 1 && $totalPageNum >= $pageColNum){
+      $minPageNum = $currentPageNum;
+      $maxPageNum = 5;
+
+      //総ページ数が表示項目数より少ない場合は、総ページ数をループマックス、ループMinを１に設定
+    }elseif($totalPageNum < $pageColNum){
+      $minPageNum = 1;
+      $maxPageNum = $totalPageNum;
+
+      //それ以外は左に２個出す。
+    }else{
+      $minPageNum = $currentPageNum -2;
+      $maxPageNum = $currentPageNum +2;
+    }
+
+    echo '<div class="pagination>';
+      echo '<ul class="pagination-list>';
+        if($currentPageNum != 1){
+          echo '<li class="list-item"><a href=""?</li>';
+        }
+        for($i = $minPageNum; $i <= $maxPageNum; $i++){
+          echo '<li class="list-item ';
+          if($currentPageNum == $i){echo 'active';}
+          echo '"><a href="?p='.$i.$link.'">'.$i.'</a></li>';
+        }
+        if($currentPageNum != $maxPageNum){
+          echo '<li class="list-item"><a href="?p='.$maxPageNum.$link.'">&gt;</a></li>';
+        }
+      echo '</ul>';
+    echo '<div>';
+}
+
+//画像表示用関数
+function shoImg($path){
+  if(empty($path)){
+    return 'img/sample-img.png';
+  }else{
+    return $path;
+  }
+}
+//GETパラメータ付与
+//$del _key : 付与から取り除きたいGETパラメータのキー
+function appendGetParam($arr_del_key){
+  if(!empty($_GET)){
+    $str = '?';
+    foreach($_GET as $key => $val){
+      if(!in_array($key,$arr_del_key,true)){//取り除きたいパラメータじゃない場合にurlにくっつけるパラメータを生成
+      }
+    }
+    $str = mb_substr($str, 0,-1, "UTF-8");
+    echo $str;
   }
 }
 ?>
